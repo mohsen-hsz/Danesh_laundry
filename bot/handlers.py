a4a4565111f69a4df6359adfa8a2b11c460a246f
Mohsen <hasanzadeh.mohsen228@gmail.com>
2025-11-10 00:34:18 +0330

    fix44

--- bot/handlers.py
+++
@@ -1,5 +0,0 @@
-from telegram import Update
-from telegram.ext import ContextTypes
-
-async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    await update.message.reply_text("✅ ربات فعاله! سلام 👋")
